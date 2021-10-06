84e31ba15f345ce13eb2b2a25514800523d01a47
RIVIERE Julien <JRIVIERE>
2021-10-06 11:54:31 +0200

    un diagrame de fonctionnement pour le client et les premices de l'API rest

--- pageWeb/includes/rest.php
+++ pageWeb/includes/rest.php
@@ -1,3 +1,19 @@
 <?php 
-    //TODO
+    session_start();
+    switch($_SERVEUR["REQUEST_METHODE"]){
+        case ("get"):
+            //TODO
+            break;
+        case ("post"):
+            //TODO
+            break;
+        case ("put"):
+            //TODO
+            break;
+        case ("delete"):
+            //TODO
+            break;
+        default:
+            //ERROR
+    }
 ?>
